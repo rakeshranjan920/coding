645bdb562244836c4aad2e89dc3d73a7cf57e9a5
Rakesh Ranjan B <rakesh.b.ranjan@ericsson.com>
2020-06-20 22:20:42 +0530

    coding

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int, int>	pint;
+typedef vector<int>		vint;
+typedef vector<long>    vlong;
+typedef vector<pint>	vector_pint;
+typedef vector<vint>	vv_int;
+typedef vector<vlong>   vv_long;
+typedef stack<int>      Stack;
+typedef map<int, int>   Map;
+#define fastio          ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+
+
+void solve() {
+
+}
+
+int main() {
+    fastio;
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
